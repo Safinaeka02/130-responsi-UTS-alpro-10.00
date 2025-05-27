d286ae6d81dc8399dad29c3006f77acec104651b
Safinaeka02 <2400018130@webmail.uad.ac.id>
2025-05-27 13:30:21 +0700

    Add files via upload

---
+++ responsi.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<iostream>
+#include<fstream>
+#include<vector>
+using namespace std;
+
+int main (){
+	string username;
+	float password;
+	float harga;
+	float diskon;
+	
+	cout<<"Masukkan username :";
+	cin >>username;
+	cout<<"Masukkan password:";
+	cin>>password;
+	
+switch(belanja){
+	case 1:
+		cout<<"masukkan nama barang 1:";
+		cin>>1
+		nama barang = buku;
+		harga = 5000;
+		break;
+	case 2:
+		cout<<"Masukkan nama barang 2:";
+		cin >>2;
+		nama barang = baju;
+		harga = 100000;
+		break;
+	case 3:
+		cout<<"Masukkan nama barang 3:";
+		cin>>3;
+		nama barang = celana;
+		harga =  150000;
+		break;
+	default;
+}
+void rincian{
+    string nama;
+	int belanja, int pelanggan;
+	ifstream()
+	}
+	cout<<"Masukkan harga:";
+	cin>>harga;
+	cout<<"Masukkan diskon :";
+	cin>>diskon;
+	cout<<"Hasil pemasukkan supermarket dalam sehari:";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
